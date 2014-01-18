82b70ce3f18b6da6462f4bc4e0e8855aaff8773c
Benji.Net <BRomeroT@hotmail.com>
2014-01-18 17:26:19 -0600

    Hubicacion autos

---
+++ js/Datos.js
@@ -0,0 +1,106 @@
+﻿var Datos = {
+
+    unidadesSelectId: "unidadesSelect",
+
+    Unidades: {},
+
+    EstacionSeleccionada: null,
+
+    Eventos: null,
+
+    EventosMapa: {
+        Pin_Click: function (pin) {
+            Datos.SeleccionarPin(pin)
+        }
+    },
+
+    Autentificar: function (usuario, contrasena, resultado) {
+        var Params = {};
+        Params.type = 'GET';
+        Params.url = 'http://localize.dyndns-ip.com/ClientBin/Localize-Web-ServicioRIA.svc/JSON/GetUsuarios?$where=((it.usuario1.Trim().ToLower()%253d%253d%2522' +
+            usuario + '%2522)%2526%2526(it.passweb.Trim()%253d%253d%2522' + contrasena + '%2522))&$includeTotalCount=True';
+        //Params.dataType = 'json';
+        Params.success = function (res) {
+            var data = $.parseJSON(res.responseText);
+            var autentificado = (data.GetUsuariosResult.TotalCount == 1);
+            if (autentificado)
+                resultado(true, data.GetUsuariosResult.RootResults[0].flotilla);
+            else
+                resultado(false, "");
+        };
+        $.ajax(Params);
+    },
+
+    SeleccionarPin: function (pin) {
+        //analiza los datos del pin y manda a seleccionar la estación
+        //Datos.SeleccionarUnidad($(pin._htmlContent).attr('id').replace('estacion', ''));
+    },
+
+    SeleccionarUnidad: function (unidadId) {
+        var unidad = Datos.Unidades[unidadId];
+        if (Datos.EstacionSeleccionada != unidad) {
+            Datos.EstacionSeleccionada = unidad;
+            //Selecciona en el Select la estación correspondiente (por si fue seleccionada en el mapa)
+            if ($("#" + Datos.estacionesSelectId).val() != unidad.Id) {
+                $("#" + Datos.estacionesSelectId + " option").removeAttr("selected");
+                $("#" + Datos.estacionesSelectId + " option[value=" + unidadId + "]").attr("selected", "selected");
+            }
+            Datos.Eventos.UnidadSeleccionada(unidad);
+        }
+    },
+
+    CargarFlotilla: function (flotilla) {
+        var Params = {};
+        Params.type = 'GET';
+        Params.url = 'http://localize.dyndns-ip.com/ClientBin/Localize-Web-ServicioRIA.svc/JSON/ObtenerVehiculosDeGrupo?grupo=' + flotilla;
+        //Params.dataType = 'json';
+        Params.success = function (res) {
+            var data = $.parseJSON(res.responseText);
+            var esPrimera = true;//para selecionar la primera estacion
+            for (var i in data.ObtenerVehiculosDeGrupoResult.RootResults) {
+                var unidad = data.ObtenerVehiculosDeGrupoResult.RootResults[i];
+                Datos.Unidades[unidad.Id] = unidad;
+                /*Llenar el select*/
+                if (esPrimera == true) {
+                    $("#" + Datos.unidadesSelectId).append('<option value="' + unidad.Id + '" selected="selected">' + unidad.Etiqueta + '</option>');
+                } else {
+                    $("#" + Datos.unidadesSelectId).append('<option value="' + unidad.Id + '">' + unidad.Etiqueta + '</option>');
+                }
+                $("#" + Datos.unidadesSelectId).change(function () {
+                    Datos.MostrarUnidad($(this).val());
+                    //Datos.SeleccionarUnidad($(this).val());
+                });
+                if (esPrimera == true) {
+                    esPrimera = false;
+                    Datos.MostrarUnidad(unidad.Id);
+                    //Datos.SeleccionarUnidad(unidad.Id);
+                }
+            }
+            Datos.Eventos.UnidadesListas();
+        };
+        $.ajax(Params);
+    },
+
+    MostrarUnidad: function (unidadId) {
+        Mapa.LimpiarDatos();
+        var unidad = Datos.Unidades[unidadId];
+        var Params = {};
+        Params.type = 'GET';
+        Params.url = 'http://localize.dyndns-ip.com/ClientBin/Localize-Web-ServicioRIA.svc/JSON/GetUltimoPuntoLogHoy?unidad=' + unidad.unidad;
+        //Params.dataType = 'json';
+        Params.success = function (res) {
+            var data = $.parseJSON(res.responseText);
+            var punto = data.GetUltimoPuntoLogHoyResult.RootResults[0];
+            Mapa.AgregarPin('<img src="img/Car.png" alt="' + punto.vel + '" />', punto.lat, punto.lon);
+            Mapa.Centrar(punto.lat, punto.lon);
+        };
+        $.ajax(Params);
+    },
+
+    Limpiar: function () {
+        $.each(Datos.Estaciones, function (index, value) {
+            $("#M-" + value.Numero).empty();
+        });
+    },
+
+}
---
+++ js/Mapa.js
@@ -0,0 +1,44 @@
+﻿var Mapa = {
+    mapaElement: null, mapaElementId: "", Eventos: null, Inicializado: false,
+    Inicializar: function (mapaElementId, Eventos, inicializado, error) {
+        Mapa.mapaElementId = mapaElementId;
+        Mapa.Eventos = Eventos;
+        try {
+            Mapa.mapaElement = new Microsoft.Maps.Map(document.getElementById(mapaElementId),
+                            {
+                                credentials: "Al9zPH654c2ilovwyubEWKEKfK0Z3q_lFHBf-OIsNRHtv49Pf0cuE3m9pVT3hImU",
+                                mapTypeId: Microsoft.Maps.MapTypeId.road,
+                                center: new Microsoft.Maps.Location(19.040899, -98.138100),
+                                zoom: 8,
+                                enableSearchLogo: false,
+                                showMapTypeSelector: false,
+                            })
+            Mapa.Inicializado = true;
+            if (inicializado != 'undefined') inicializado();
+        } catch (e) {
+            Mapa.Inicializado = false;
+            if (error != 'undefined') error(e);
+        }
+    },
+
+    AgregarPin: function (html, latitud, longitud) {
+        var pos = new Microsoft.Maps.Location(latitud, longitud)
+        var pin = new Microsoft.Maps.Pushpin(pos, { htmlContent: html});
+        Mapa.mapaElement.entities.push(pin);
+        Microsoft.Maps.Events.addHandler(pin, 'click', function (e) {
+            if (Mapa.Eventos != null && Mapa.Eventos.Pin_Click != 'undefined') Mapa.Eventos.Pin_Click(e.target);
+        });
+        return pin;
+    },
+
+    LimpiarDatos: function () {
+        Mapa.mapaElement.entities.clear();
+    },
+
+    Centrar: function (latitud, longitud) {
+        var opciones = Mapa.mapaElement.getOptions();
+        opciones.center = new Microsoft.Maps.Location(latitud, longitud);
+        Mapa.mapaElement.setView(opciones);
+    }
+
+};
